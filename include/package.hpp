fc15dab63070abd4a79d1649c46829f53971cf43
Maciek Stadler <mastadler100@gmail.com>
2026-01-20 14:17:41 +0100

    Pelna funkcjonalnosc polproduktow i kolejek

--- include/package.hpp
+++ include/package.hpp
@@ -3,7 +3,25 @@
 #define PACKAGE_HPP
 #include "types.hpp"
 
+#include <set>
+
 class Package {
+public:
+    Package();
+    Package(ElementID id);
+    Package(Package&& paka);
+    
+    Package& operator= (Package&& paka);
+    ElementID get_id() const {return id_;}
+
+
+    ~Package();
+
+
+private:
+    ElementID id_;
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
 
 };
 
--- src/package.cpp
+++ src/package.cpp
@@ -1,1 +1,57 @@
 #include "package.hpp"
+
+std::set<ElementID> Package::assigned_IDs;
+std::set<ElementID> Package::freed_IDs;
+
+
+Package::Package(){
+    if (freed_IDs.empty()){
+        if (assigned_IDs.empty()) id_ = 1;
+        else id_ = *assigned_IDs.rbegin() + 1;
+        
+    }
+    else {
+        id_ = *freed_IDs.begin();
+        freed_IDs.erase(id_);
+    }
+    assigned_IDs.insert(id_);
+    
+    
+}
+
+Package::Package(ElementID id) : id_(id){
+    assigned_IDs.insert(id);
+    freed_IDs.erase(id);
+}
+
+
+Package::Package(Package&& paka){
+    
+    id_ = paka.get_id();
+    paka.id_ = 0;
+}
+
+Package& Package::operator= (Package&& paka){
+
+    if (this == &paka) return *this;
+
+    if (id_ != 0){
+        assigned_IDs.erase(id_);
+        freed_IDs.insert(id_);        
+    }
+    id_ = paka.id_;
+    paka.id_ = 0;
+    
+    return *this;
+
+}
+
+
+
+
+Package::~Package(){
+    if (id_ != 0){
+        assigned_IDs.erase(id_);
+        freed_IDs.insert(id_);
+    }
+}
