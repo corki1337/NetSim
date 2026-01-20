13be64eb7e7a7306bd4a30345bbd8ae88191b4d5
Kamil <kamil.smolarczyk20@gmail.com>
2026-01-20 21:35:06 +0100

    Dodanie klasy Receiver Preference

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -7,7 +7,9 @@
 
 #include "types.hpp"
 #include "storage_types.hpp"
+#include "helpers.hpp"
 #include <memory>
+#include <map>
 
 class IPackageReceiver{
 public:
@@ -16,6 +18,30 @@ public:
     virtual ~IPackageReceiver() = default;
 };
 
+class ReceiverPreferences {
+    
+    public:
+        using preferences_t = std::map<IPackageReceiver*, double>;
+        using const_iterator = preferences_t::const_iterator;
+
+        explicit ReceiverPreferences(ProbabilityGenerator pg = probability_generator) : pg_(pg) {}
+
+        void add_receiver(IPackageReceiver* receiver);
+        void remove_receiver(IPackageReceiver* receiver);
+        IPackageReceiver* choose_receiver();
+        const preferences_t& get_preferences() const{
+            return preferences_;
+        }
+
+        const_iterator begin() const;
+        const_iterator cbegin() const;
+        const_iterator end() const;
+        const_iterator cend() const;
+
+    private:
+    preferences_t preferences_; 
+    ProbabilityGenerator pg_; 
+};
 
 
 #endif
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,1 +1,49 @@
 #include "nodes.hpp"
+
+void ReceiverPreferences::add_receiver(IPackageReceiver* receiver) {
+    
+    preferences_[receiver] = 0.0; 
+    double new_prob = 1.0 / preferences_.size(); 
+    for (auto& pair : preferences_) {
+        pair.second = new_prob;
+    }
+}
+
+void ReceiverPreferences::remove_receiver(IPackageReceiver* receiver) {
+
+    if (preferences_.erase(receiver) > 0 && !preferences_.empty()) {
+        double new_prob = 1.0 / preferences_.size();
+        for (auto& item : preferences_) {
+            item.second = new_prob;
+        }
+    }
+}
+
+IPackageReceiver* ReceiverPreferences::choose_receiver() {
+    double p = pg_();
+    double distribution = 0.0;
+
+    for (const auto& item : preferences_) {
+        distribution += item.second;
+        if (p <= distribution) {
+            return item.first;
+        }
+    }
+
+    return nullptr;
+}
+ReceiverPreferences::const_iterator ReceiverPreferences::begin() const {
+    return preferences_.begin();
+}
+
+ReceiverPreferences::const_iterator ReceiverPreferences::cbegin() const {
+    return preferences_.cbegin();
+}
+
+ReceiverPreferences::const_iterator ReceiverPreferences::end() const {
+    return preferences_.end();
+}
+
+ReceiverPreferences::const_iterator ReceiverPreferences::cend() const {
+    return preferences_.cend();
+}
