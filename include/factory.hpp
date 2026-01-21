54adb5e31f1f5ac00f9533d7b8ceb7c6e3da5dc0
Maciek Stadler <mastadler100@gmail.com>
2026-01-21 18:01:17 +0100

    Dodano straznika naglowka do factory

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,3 +1,29 @@
 //szablon klasy NodeCollection
 //klasa Factory
 //funkcje do zapisu i odczytu struktury fabryki z pliku
+
+#ifndef FACTORY_HPP
+#define FACTORY_HPP
+
+
+#include "nodes.hpp"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
