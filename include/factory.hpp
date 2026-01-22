6aac8263b838876fdc68ec8ee763462e578c7d95
Maciek Stadler <mastadler100@gmail.com>
2026-01-22 19:19:38 +0100

    Dodano deklaracje metod klasy NodeCollection

--- include/factory.hpp
+++ include/factory.hpp
@@ -9,7 +9,37 @@
 #include "nodes.hpp"
 
 
+template <class Node>
 
+class NodeCollection{
+public:
+
+    using container_t = typename std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+
+
+    const_iterator cbegin() const {return konteneruch_.cbegin();}
+    const_iterator cend() const {return konteneruch_.cend();}
+    const_iterator begin() const {return konteneruch_.begin();}
+    const_iterator end() const {return konteneruch_.end();}
+    iterator begin() {return konteneruch_.begin();}
+    iterator end() {return konteneruch_.end();}
+
+    void add(Node&& node) {konteneruch_.push_back(std::move(node));}
+    void remove_by_id(ElementID id);
+    iterator find_by_id(ElementID id);
+    const_iterator find_by_id(ElementID id) const;
+
+
+
+
+
+private:
+    container_t konteneruch_;
+
+
+};
 
 
 
