3c663123053f730db97a9a4af617f93dca96fffa
KSmolarczyk999 <kamil.smolarczyk20@gmail.com>
2026-01-24 16:58:15 +0100

    Pełna implementacja klasy Factory

--- include/factory.hpp
+++ include/factory.hpp
@@ -8,6 +8,7 @@
 
 #include "nodes.hpp"
 #include <algorithm>
+#include <stdexcept>
 
 template <class Node>
 
@@ -46,12 +47,25 @@ public:
     NodeCollection<Ramp>::const_iterator ramp_cbegin() const {return ramps.cbegin();}
     NodeCollection<Ramp>::const_iterator ramp_cend() const {return ramps.cend();}
   
-    //TODO Dla workerów i storehouses jak wyzej
-    //TODO Pozostale funkcje takie jak:
-    //is_consistent(void) : bool {query}
-    //do_deliveries(Time) : void
-    //do_package_passing(void) : void
-    //do_work(Time) : void
+    void add_worker(Worker&& worker) {workers.add(std::move(worker));}
+    void remove_worker(ElementID id) {remove_receiver(workers, id);}
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator worker_cbegin() const {return workers.cbegin();}
+    NodeCollection<Worker>::const_iterator worker_cend() const {return workers.cend();}
+  
+    void add_storehouse(Storehouse&& storehouse) {storehouses.add(std::move(storehouse));}
+    void remove_storehouse(ElementID id) {remove_receiver(storehouses, id);}
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const {return storehouses.cbegin();}
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() const {return storehouses.cend();}
+    
+    bool is_consistent() const;
+    void do_deliveries(Time t);
+    void do_package_passing();
+    void do_work(Time t);
+    
 private:
 
     template <typename Node>
@@ -63,6 +77,25 @@ private:
     NodeCollection<Storehouse> storehouses;
 };
 
+
+template <typename Node>
+void Factory::remove_receiver(NodeCollection<Node>& collection, ElementID id){
+
+    typename NodeCollection<Node>::iterator iterator = collection.find_by_id(id);
+
+    if (iterator !=collection.end()) {
+
+        IPackageReceiver* receiver_ptr = &(*iterator);
+        for( auto& ramp: ramps){
+            ramp.receiver_preferences_.remove_receiver(receiver_ptr);
+        }
+        for( auto& worker:workers)
+            {worker.receiver_preferences_.remove_receiver(receiver_ptr);            
+        }
+    
+        collection.remove_by_id(id);
+    }
+}
 
 
 
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,2 +1,89 @@
 #include "factory.hpp"
 
+bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors) {
+
+    if (node_colors[sender] == NodeColor::VERIFIED) {
+        return true;
+    }    
+    node_colors[sender] = NodeColor::VISITED; 
+
+    if (sender->receiver_preferences_.get_preferences().empty()) {
+        throw std::logic_error("No defined receivers");
+    }
+    bool has_other_receiver = false;
+
+    for(const auto& receiver: sender->receiver_preferences_.get_preferences()){
+    
+        if(receiver.first->get_receiver_type() == ReceiverType::STOREHOUSE){
+            has_other_receiver = true;
+        }
+        
+        else if(receiver.first->get_receiver_type() == ReceiverType::WORKER){
+            
+            IPackageReceiver* receiver_ptr = receiver.first;
+            auto worker_ptr = dynamic_cast<Worker*>(receiver_ptr);
+            auto sendrecv_ptr =dynamic_cast<PackageSender*>(worker_ptr);
+
+            if (worker_ptr == sender){
+                continue;
+            }
+            has_other_receiver = true;
+
+            if(node_colors[sendrecv_ptr] == NodeColor::UNVISITED){
+                has_reachable_storehouse(sendrecv_ptr, node_colors);
+            }
+        }   
+    }
+    node_colors[sender] = NodeColor::VERIFIED;
+
+    if (has_other_receiver) {
+        return true;
+    } 
+    else {
+        throw std::logic_error("No reachable magazine");
+    }
+}  
+
+bool Factory::is_consistent() const {
+    std::map<const PackageSender*, NodeColor> node_colors;
+
+    for (const auto& ramp : ramps) {
+        node_colors[&ramp] = NodeColor::UNVISITED;
+    }
+    for (const auto& worker : workers){
+        node_colors[&worker] = NodeColor::UNVISITED;
+    }
+
+    try{        
+        for (const auto& ramp : ramps) {
+            has_reachable_storehouse(&ramp, node_colors);
+        }
+    }
+    catch (const std::logic_error&) {
+        return false;
+    }
+
+    return true;
+}
+
+void Factory::do_deliveries(Time t){
+    for (auto& ramp: ramps){
+        ramp.deliver_goods(t);
+    }
+}
+
+void Factory::do_package_passing() {
+    for (auto& ramp: ramps){
+        ramp.send_package();
+    }
+
+    for (auto& worker: workers){
+        worker.send_package();
+    }
+}
+
+void Factory::do_work(Time t){
+    for (auto& worker: workers){
+        worker.do_work(t);
+    }
+}
